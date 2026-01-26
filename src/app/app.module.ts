3a20c6ee044de378306c4e29964a18cc6b7ab132
DESKTOP-VU9GHEL\trubl <trublereau@msn.com>
2026-01-26 21:32:38 +0100

    Add newsletter via template-driven form (FormsModule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 
@@ -22,7 +23,8 @@ import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'fr-FR' }
